a8775c8b0dcf5dcf70c41168c2da94d1c70c65e0
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2024-08-06 00:53:50 +0900

    feat
    로그인 테스트

--- user/src/App.js
+++ user/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './component/member';
+import { Main } from './component/main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/main' element={<Main/>} />
+    </Routes>
+    </>
   );
 }
 
---
+++ user/src/component/member/Login.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+    const [userid , setUserid] = useState("");
+    const [pwd , setPwd] = useState("");
+    const navigate = useNavigate();
+
+    let onLogin = async()=>{
+       try{
+            let usernameWithRole =userid+":user";
+            let result = await axios.post("/api/member/login",null,{params:{usernameWithRole,password:pwd}});
+            
+            if(result.data.error == "ERROR_LOGIN"){
+              return alert(result.data.error+"이메일 또는 패드스워드 오류입니다.");
+            }else{
+              console.log(result.data);
+              navigate("/main");
+            }
+       }catch(err){
+            console.error(err);
+       }
+    }
+
+  return (
+    <div>
+        아이디 : <br/>
+      <input type="text" name="userid" value={userid} onChange={
+        (e)=>{setUserid(e.currentTarget.value)}
+      } /><br/>
+      비밀번호 : <br/>
+      <input type="text" name="pwd"  value={pwd} onChange={
+        (e)=>{setPwd(e.currentTarget.value)}
+      }  /><br/>
+      <button onClick={onLogin}>로그인</button>
+    </div>
+  )
+}
+
+export default Login
