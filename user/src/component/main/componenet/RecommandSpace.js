a4bac5b0fbf1000cb4ac174a87e9a54b9a5ccca6
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2024-08-19 17:18:15 +0900

    FEAT

--- user/src/component/main/componenet/RecommandSpace.js
+++ user/src/component/main/componenet/RecommandSpace.js
@@ -3,7 +3,10 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/scrollbar';
 import { Scrollbar } from 'swiper/modules';
 import jaxios from '../../../util/jwtUtil';
+import 'swiper/css/scrollbar';
+
 import axios from 'axios';
+import SpaceBoxComponent from './SpaceBoxComponent';
 
 
 function RecommandSpace({user}) {
@@ -13,7 +16,6 @@ function RecommandSpace({user}) {
         getRecommandSpace();
     },[user]);
     
-
     let getRecommandSpace=async()=>{
         try{
             
@@ -21,6 +23,7 @@ function RecommandSpace({user}) {
             if(user.email){ result = await axios.post("/api/main/getRecommandSpace",user)}
             else { result = await axios.post("/api/main/getRecommandSpace")}
             console.log(result.data);
+            setRcList(result.data);
         }catch(err){console.error(err);}
     }
 
@@ -30,15 +33,20 @@ function RecommandSpace({user}) {
         <h2>추천 공간</h2>
         <p>이런 공간은 어떠세요?!</p>
       </div>
-      <Swiper slidesPerView={3} spaceBetween={30} grabCursor={true} breakpoints={{
-            769: {
-              slidesPerView: 2,
-              slidesPerGroup: 2,
-            },}}
-        scrollbar={true} modules={[Scrollbar]} className="rcommandSlide" >
-        <SwiperSlide>
-            
-        </SwiperSlide>
+      <Swiper slidesPerView={1.5} spaceBetween={30} grabCursor={true} 
+            scrollbar={{ hide: false }}
+            modules={[Scrollbar]}
+            breakpoints={{
+              769: {
+                slidesPerView: 3,
+                slidesPerGroup: 1,
+              },}}
+            className="rcommandSlide" >
+        {(rcList)&&(rcList.map((elem, idx)=>(
+          <SwiperSlide key ={idx}>
+            <SpaceBoxComponent record={elem} />
+          </SwiperSlide>
+        )))}
       </Swiper>
     </div>
   )
