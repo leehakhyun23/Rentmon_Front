93ebdea21c6b92d8df8275b2fa5a7f25c5c2b123
Joungbin Park <163955135+JoungbinPark@users.noreply.github.com>
2024-08-19 15:32:59 +0900

    FIx_ 오류 정리

--- user/src/App.js
+++ user/src/App.js
@@ -1,51 +1,50 @@
 import './App.css';
-import {Route, Router, Routes} from 'react-router-dom';
-import {Choosecategory, Getsnsuserinfo, JoinForm, Login} from './component/member';
-import {Main} from './component/main';
+import { Route, Router, Routes } from 'react-router-dom';
+import { Choosecategory, Getsnsuserinfo, JoinForm, Login } from './component/member';
+import { Main } from './component/main';
 import Header from './component/headerfooter/Header';
 import Footer from './component/headerfooter/Footer';
 import SpaceList from './component/space/SpaceList';
 import SpaceDetail from './component/space/SpaceDetail';
+import { Coupon, DashBoard, Mypage, Reservation } from './component/mypage';
+
 import ReservationForm from './component/reservation/ReservationForm';
 import ReservationDone from './component/reservation/ReservationDone';
-import Getsnsuserinfo from './component/member/Getsnsuserinfo';
-import {Coupon, DashBoard, Mypage, Reservation} from './component/mypage';
 
-
-import ReservationForm from './component/reservation/ReservationForm';
 import Usesapce from './component/mypage/Usesapce';
 import Zzim from './component/mypage/Zzim';
 import Qna from './component/mypage/Qna';
 import Updateuser from './component/mypage/Updateuser';
 
 function App() {
-    return (
-        <>
-        <Header/>
-        <Routes>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/' element={<Main/>}/>
-            <Route path='/spaceList' element={<SpaceList/>}/>
-            <Route path='/spaceDetail/:sseq' element={<SpaceDetail/>}/>
-            <Route path='/getsnsuserinfo/:userid/:provider' element={<Getsnsuserinfo/>}/>
-            <Route path='/join' element={<JoinForm/>}/>
-            <Route path='/choosecategory/:userid' element={<Choosecategory/>}/>
-            <Route path='/reservationForm/:sseq' element={<ReservationForm/>}/>
-            <Route path='/reservationDone/:sseq' element={<ReservationDone/>}/>
-            <Route path='/mypage' element={<Mypage/>}/>
-            <Route path='dashboard' element={<DashBoard/>}/>
-            <Route path='coupon/:currentPage' element={<Coupon/>}/>
-            <Route path='reservation/:currentPage' element={<Reservation/>}/>
-            <Route path='usesapce/:currentPage' element={<Usesapce/>}/>
-            <Route path='zzim' element={<Zzim/>}/>
-            <Route path='qna/:currentPage' element={<Qna/>}/>
-            <Route path='updateuser' element={<Updateuser/>}/>
-        </Route>
-        </Routes>
-    <Footer/>
-    < />
-)
-    ;
+  return (
+    <>
+    <Header />
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<Main/>} />
+      <Route path='/spaceList' element={<SpaceList />} />
+      <Route path='/spaceDetail/:sseq' element={<SpaceDetail />} />
+      <Route path='/getsnsuserinfo/:userid/:provider' element={<Getsnsuserinfo/>}/>
+      <Route path='/join' element={<JoinForm />} />
+      <Route path='/choosecategory/:userid' element={<Choosecategory />} />
+      <Route path='/reservationForm/:sseq' element={<ReservationForm />} />
+      <Route path='/reservationDone/:sseq' element={<ReservationDone />} />
+
+      <Route path='/mypage' element={<Mypage />}>
+        <Route path='dashboard' element={<DashBoard/>}/>
+        <Route path='coupon/:currentPage' element={<Coupon/>}/>
+        <Route path='reservation/:currentPage' element={<Reservation/>}/>
+        <Route path='usesapce/:currentPage' element={<Usesapce/>} />
+        <Route path='zzim' element={<Zzim/>} />
+        <Route path='qna/:currentPage' element={<Qna/>} />
+        <Route path='updateuser' element={<Updateuser/>} />
+      </Route>
+    </Routes>
+    <Footer />
+
+    </>
+  );
 }
 
 export default App;
